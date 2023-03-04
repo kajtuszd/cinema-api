a11bad3cd0a66e0de8b379021b7b17608985f81b
Kajetan Zdanowicz <kajetanzdanowicz@wp.pl>
2023-03-04 13:38:32 +0100

    db connection handler, basic user model

--- app/main/main.go
+++ app/main/main.go
@@ -1,73 +1,53 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/kajtuszd/cinema-api/app/database"
+	"github.com/kajtuszd/cinema-api/app/models"
 	"net/http"
-	"os"
 )
-
-type User struct {
-	gorm.Model
-	Name string
-}
 
 func main() {
 	r := gin.Default()
+	db := &database.GormDatabase{}
+	err := db.Connect()
+	if err != nil {
+		panic("Can not connect to database")
+	}
+	defer db.Close()
+	if err != nil {
+		panic(err)
+	}
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("Error loading .env file")
-	}
-
-	dbUser := os.Getenv("POSTGRES_USER")
-	dbPassword := os.Getenv("POSTGRES_PASSWORD")
-	dbName := os.Getenv("POSTGRES_DB")
-	dbHost := os.Getenv("POSTGRES_HOST")
-	dbPort := os.Getenv("POSTGRES_PORT")
-	dbSSLMode := os.Getenv("SSLMODE")
-
-	dsn := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=%s",
-		dbUser, dbPassword, dbName, dbHost, dbPort, dbSSLMode)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("No error")
-	}
-
-	err = db.AutoMigrate(&User{})
-	if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("Migrations done")
-	}
 
 	r.GET("/", func(c *gin.Context) {
-		var users []User
-		db.Find(&users)
+		var users []models.User
+		db.DB().Find(&users)
 
 		c.JSON(http.StatusOK, users)
 	})
 
 	r.POST("/users", func(c *gin.Context) {
-		var user User
+		var user models.User
 		if err := c.ShouldBindJSON(&user); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-		if err := db.Create(&user).Error; err != nil {
+		if err := db.DB().Create(&user).Error; err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
 		c.JSON(http.StatusOK, user)
 	})
+
+	err = db.Migrate()
+	if err != nil {
+		panic(err)
+	}
 	r.Run()
 }
